bbd624a60b48c221535576525bb3ed1594fe3476
john stone <john@tuxcode.org>
2012-07-18 02:34:15 +0200

    testpwmcode

--- firmware/main.c
+++ firmware/main.c
@@ -6,6 +6,24 @@
 
 #include "main.h"
 
+
+void init_pwm(void){
+	
+	// TCCR1A xx01xx10
+	TCCR1A &= ~0b00100001;
+	TCCR1A |=  0b00000010;
+
+	// TCCR1B xxx11001
+	TCCR1B &= ~0b00000110;
+	TCCR1B |=  0b00011001;
+	
+	//PB1 set to output:
+	DDRB |= 0b10;
+
+
+}
+
+
 int main (void)
 {
 
@@ -22,17 +40,29 @@ int main (void)
 	//reduce power
 	PRR |= (1<<PRTWI)|(1<<PRTIM2)|(1<<PRSPI)|(1<<PRUSART0);
 
-	PORTB |= (1 << PORTB1);
+	/*PORTB |= (1 << PORTB1);*/
 	PORTD = 0;
 
+	init_pwm();
 
-	while(1)
-	{
-		_delay_ms(500);
-		PORTC ^= (1<<PORTC0)|(1<<PORTC2);
-		PORTB ^= (1<<PORTB1);
-		PORTD ^= (1<< PORTD6);
 
+	/*while(1)*/
+	/*{*/
+		/*_delay_ms(500);*/
+		/*PORTC ^= (1<<PORTC0)|(1<<PORTC2);*/
+		/*PORTB ^= (1<<PORTB1);*/
+		/*PORTD ^= (1<< PORTD6);*/
+
+	/*}*/
+
+	for(;;){
+		OCR1BH = 0;
+		OCR1BL = 0;
+		_delay_ms(1);
+		OCR1BH = 0xff;
+		OCR1BL = 0xff;
+		_delay_ms(1);
 	}
+
 }
 
