e4f778d01e2cd9ea19f27c0f7ede476b820a75e3
Thammi <thammi@chaossource.net>
2013-09-05 02:05:23 +0200

    Add octave switching to pentatonic app

--- firmware/include/pentabug/helper.h
+++ firmware/include/pentabug/helper.h
@@ -3,4 +3,9 @@
 
 #define ARRAY_SIZE(array)		(sizeof(array)/sizeof(*array))
 
+#define MAX(a, b)			(a > b ? a : b)
+#define MIN(a, b)			(a < b ? a : b)
+
+#define ever				(;;)
+
 #endif /* HELPER_H */
